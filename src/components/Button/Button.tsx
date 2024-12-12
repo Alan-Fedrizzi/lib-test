7ace5f1a084041a19beb393eca9f3c3f29efcf9b
alanfedrizzi <alanfedrizzi@gmail.com>
2024-12-12 14:23:16 +0100

    teste de commit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,22 @@
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
 export default function TpButton() {
   return <Button variant="contained">aahhhh</Button>;
 }
+
+/*
+// import { Button } from '@mui/material';
+import { Button } from "react-native";
+// teste
+export default function TpButton() {
+  // return <Button variant="contained">aahhhh</Button>;
+  return (
+    <Button
+      onPress={() => console.log("ahhh")}
+      title="Learn More"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+    />
+  );
+}
+*/
